8ccf0342448e46a284acad0125dd43f5a35a970a
BuildTools <unconfigured@null.spigotmc.org>
2024-05-12 22:31:51 -0700

    Developing Sections

--- src/script/title.js
+++ src/script/title.js
@@ -7,6 +7,7 @@ let colorTrainStartLetter = {
   yellow: -5,
   purple: -6,
   orange: -7,
+  green: -8,
 }
 
 function changeColor(colorTrainStartLetter) {
@@ -32,8 +33,11 @@ function changeColor(colorTrainStartLetter) {
     else if (i == colorTrainStartLetter.orange) {
       titleCharacters.item(i).style.color = "#fa0"
     }
-    else {
+    else if (i == colorTrainStartLetter.green) {
       titleCharacters.item(i).style.color = "#0f0"
+    }
+    else {
+      titleCharacters.item(i).style.color = "#fff"
     }
   }
 }
@@ -42,24 +46,34 @@ function addToColors(colorTrainStartLetter) {
   colorTrainStartLetter.red++;
   if (colorTrainStartLetter.red > titleCharacters.length)
     colorTrainStartLetter.red = 0;
+
   colorTrainStartLetter.blue++;
   if (colorTrainStartLetter.blue > titleCharacters.length)
     colorTrainStartLetter.blue = 0;
+
   colorTrainStartLetter.pink++;
   if (colorTrainStartLetter.pink > titleCharacters.length)
     colorTrainStartLetter.pink = 0;
+
   colorTrainStartLetter.cyan++;
   if (colorTrainStartLetter.cyan > titleCharacters.length)
     colorTrainStartLetter.cyan = 0;
+
   colorTrainStartLetter.yellow++;
   if (colorTrainStartLetter.yellow > titleCharacters.length)
     colorTrainStartLetter.yellow = 0;
+
   colorTrainStartLetter.purple++;
   if (colorTrainStartLetter.purple > titleCharacters.length)
     colorTrainStartLetter.purple = 0;
+
   colorTrainStartLetter.orange++;
   if (colorTrainStartLetter.orange > titleCharacters.length)
     colorTrainStartLetter.orange = 0;
+
+  colorTrainStartLetter.green++;
+  if (colorTrainStartLetter.green > titleCharacters.length)
+    colorTrainStartLetter.green = 0;
 }
 
 setInterval(() => {
