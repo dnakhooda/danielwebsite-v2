f127a5cc00efb27678a026fcb5759a64ff476f7f
BuildTools <unconfigured@null.spigotmc.org>
2024-05-18 22:53:55 -0700

    More Small Changes

--- src/script/Home/title.js
+++ src/script/Home/title.js
@@ -1,47 +1,25 @@
 let titleCharacters = document.getElementsByClassName("full_screen_section")[0].children
 let colorTrainStartLetter = {
-  red: -1,
-  blue: -2,
-  pink: -3,
-  cyan: -4,
-  yellow: -5,
-  purple: -6,
-  orange: -7,
-  green: -8,
+  red: [-1, "#f00"],
+  blue: [-2, "#00f"],
+  pink: [-3, "#f0f"],
+  cyan: [-4, "#0ff"],
+  yellow: [-5, "#ff0"],
+  purple: [-6, "#a0f"],
+  orange: [-7, "#fa0"],
+  green: [-8, "#0f0"],
+  white: [-9, "#fff"]
 }
 
 function changeColor(colorTrainStartLetter) {
-  for (let i = 0; i < titleCharacters.length; i++) {
-    if (i == colorTrainStartLetter.red)
-      titleCharacters.item(i).style.color = "#f00"
-    else if (i == colorTrainStartLetter.blue)
-      titleCharacters.item(i).style.color = "#00f"
-    else if (i == colorTrainStartLetter.pink)
-      titleCharacters.item(i).style.color = "#f0f"
-    else if (i == colorTrainStartLetter.cyan)
-      titleCharacters.item(i).style.color = "#0ff"
-    else if (i == colorTrainStartLetter.yellow)
-      titleCharacters.item(i).style.color = "#ff0"
-    else if (i == colorTrainStartLetter.purple)
-      titleCharacters.item(i).style.color = "#a0f"
-    else if (i == colorTrainStartLetter.orange)
-      titleCharacters.item(i).style.color = "#fa0"
-    else if (i == colorTrainStartLetter.green)
-      titleCharacters.item(i).style.color = "#0f0"
-    else
-      titleCharacters.item(i).style.color = "#fff"
-  }
-}
-
-function addToColors(colorTrainStartLetter) {
   for (const property in colorTrainStartLetter) {
-    colorTrainStartLetter[property]++;
-    if (colorTrainStartLetter[property] > titleCharacters.length)
-      colorTrainStartLetter[property] = 0
+    colorTrainStartLetter[property][0]++;
+    if (colorTrainStartLetter[property][0] > titleCharacters.length - 1)
+      colorTrainStartLetter[property][0] = 0
+
+    if (colorTrainStartLetter[property][0] > -1)
+      titleCharacters.item(colorTrainStartLetter[property][0]).style.color = colorTrainStartLetter[property][1]
   }
 }
 
-setInterval(() => {
-  addToColors(colorTrainStartLetter)
-  changeColor(colorTrainStartLetter)
-}, 375)
+setInterval(() => changeColor(colorTrainStartLetter), 375)
