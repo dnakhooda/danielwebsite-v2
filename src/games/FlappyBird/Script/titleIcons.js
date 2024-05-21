6d76ba5e1d14db2b91d21641652f1fddef2d4c10
Daniel Nakhooda <dnakhooda@gmail.com>
2024-05-20 18:08:30 -0700

    Add Title Icons

---
+++ src/games/FlappyBird/Script/titleIcons.js
@@ -0,0 +1,15 @@
+let string = "DANIEL WEBSITE ";
+let i = 0;
+
+setInterval(changeTitle, 1000);
+
+function changeTitle() {
+  i++;
+  if (i > string.length - 1)
+    i = 0;
+
+  if (string[i] == ' ')
+    document.getElementById("icon").href = `../../public/TitleIcons/Space.png`;
+  else
+    document.getElementById("icon").href = `../../public/TitleIcons/${string[i]}.png`;
+}
